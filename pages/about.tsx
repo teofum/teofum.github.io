42cda025ed2aefbfe0c03c15bbff2009af9b6c23
Teo Fumagalli <teo.fum@gmail.com>
2022-03-28 02:00:21 -0300

    About page content

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,6 +5,7 @@ import Paragraph from '../components/Paragraph';
 import { homeCrumb } from '../types/Crumbs';
 
 import styles from '../styles/module/About.module.scss';
+import Link from 'next/link';
 
 const About: NextPage = () => {
   return (
@@ -16,7 +17,11 @@ const About: NextPage = () => {
         </h2>
 
         <Paragraph>
-          I&#39;m a web designer and frontend developer from Buenos Aires, Argentina. I make and write stuff on the web, 
+          I&#39;m a web designer and frontend developer from Buenos Aires, Argentina. I make and write stuff on the web, trying out some of the fun things websites (and computers) can do and explaining how they work.
+        </Paragraph>
+
+        <Paragraph>
+          I don&#39;t do social media, but you can find my stuff on <Link href='/projects'>my projects page</Link>, its source code (and this website&#39;s!) on <a href='https://github.com/teofum'>GitHub</a> and <a href='mailto:teo.fum@outlook.com'>email me</a> if you have something you want to say. Thanks for checking out my site, and have a nice day :)
         </Paragraph>
       </div>
     </BaseLayout>
