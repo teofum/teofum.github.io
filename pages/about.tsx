24e7c84405f4e349daa8d1430e56d3ce908f3f41
Teo Fumagalli <teo.fum@gmail.com>
2022-03-29 00:55:05 -0300

    Better picture of me in about page. Now you can see my face!

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,6 +6,8 @@ import { homeCrumb } from '../types/Crumbs';
 
 import styles from '../styles/module/About.module.scss';
 import Link from 'next/link';
+import ThatsMeSVG from '../components/about/ThatsMeSVG';
+import Anchor from '../components/Anchor';
 
 const About: NextPage = () => {
   return (
@@ -22,14 +24,15 @@ const About: NextPage = () => {
           </Paragraph>
 
           <Paragraph>
-            I don&#39;t do social media, but you can find my stuff on <Link href='/projects'>my projects page</Link>, its source code (and this website&#39;s!) on <a href='https://github.com/teofum'>GitHub</a> and <a href='mailto:teo.fum@outlook.com'>email me</a> if you have something you want to say. Thanks for checking out my site, and have a nice day :)
+            I don&#39;t do social media, but you can find my stuff on <Link href='/projects'>my projects page</Link>, its source code (and this website&#39;s!) on <Anchor href='https://github.com/teofum'>GitHub</Anchor>, or <a href='mailto:teo.fum@outlook.com'>email me</a> if you want to chat. Thanks for checking out my site, and have a nice day :)
           </Paragraph>
         </div>
         <div className={styles.image}>
+          <ThatsMeSVG />
           <picture>
-            <source srcSet='/content/about/me.webp' type='image/webp' />
-            <source srcSet='/content/about/me.png' type='image/png' />
-            <img src='/content/about/me.png' alt='Me :)' />
+            <source srcSet='/content/about/portrait.webp' type='image/webp' />
+            <source srcSet='/content/about/portrait.png' type='image/png' />
+            <img src='/content/about/portrait.png' alt='Me :)' />
           </picture>
         </div>
       </div>
